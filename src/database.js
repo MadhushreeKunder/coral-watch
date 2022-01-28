c262bc60ff04c07c5cc3734085a82e6b41457f21
MadhushreeKunder <madhushreekunder@gmail.com>
2022-01-28 17:13:46 +0530

    add more videos

--- src/database.js
+++ src/database.js
@@ -1,11 +1,28 @@
 export const videosDb = [
-    { id: 1, videoId: "QfLrjNDKe5g" },
-    { id: 2, videoId: "wHTcrmhskto" },
+    { id: 1, videoId: "uzkD5SeuwzM" },
+    { id: 2, videoId: "SUelbSa-OkA" },
     { id: 3, videoId: "saWNMPL5ygk" },
     { id: 4, videoId: "ThDYazipjSI" },
-    { id: 5, videoId: "SUelbSa-OkA" },
+    { id: 5, videoId: "_1R_pg5QasA" },
     { id: 6, videoId: "6SrsZVdU740" },
     { id: 7, videoId: "uD4izuDMUQA" },
     { id: 8, videoId: "dFCbJmgeHmA" },
+
+    { id: 9, videoId: "BickMFHAZR0" },
+    { id: 10, videoId: "tH2tKigOPBU" },
+    { id: 11, videoId: "DTvS9lvRxZ8" },
+    { id: 12, videoId: "xaQJbozY_Is" },
+    { id: 13, videoId: "e-P5IFTqB98" },
+    { id: 14, videoId: "pP44EPBMb8A" },
+    
+    { id: 15, videoId: "NtQkz0aRDe8" }, 
+    { id: 16, videoId: "lARpY0nIQx0" }, 
   ];
   
+  // https://www.youtube.com/watch?v=DTvS9lvRxZ8
+  // https://www.youtube.com/watch?v=wNDGgL73ihY
+  // https://www.youtube.com/watch?v=e-P5IFTqB98
+  // https://www.youtube.com/watch?v=pP44EPBMb8A
+  // https://www.youtube.com/watch?v=NtQkz0aRDe8
+  // https://www.youtube.com/watch?v=lARpY0nIQx0
+  // https://www.youtube.com/watch?v=_1R_pg5QasA

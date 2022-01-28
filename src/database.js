6817bfe0d14ded196e6f1bd9dc612a3253947371
MadhushreeKunder <madhushreekunder@gmail.com>
2022-01-28 18:17:34 +0530

    add data to db

--- src/database.js
+++ src/database.js
@@ -1,28 +1,203 @@
 export const videosDb = [
-    { id: 1, videoId: "uzkD5SeuwzM" },
-    { id: 2, videoId: "SUelbSa-OkA" },
-    { id: 3, videoId: "saWNMPL5ygk" },
-    { id: 4, videoId: "ThDYazipjSI" },
-    { id: 5, videoId: "_1R_pg5QasA" },
-    { id: 6, videoId: "6SrsZVdU740" },
-    { id: 7, videoId: "uD4izuDMUQA" },
-    { id: 8, videoId: "dFCbJmgeHmA" },
+  { id: 1,
+    videoId: "uzkD5SeuwzM",
+    videoTitle: "TRUE Limits Of Humanity – The Final Border We Will Never Cross",
+    views: "13,792,104",
+    date: "May 11, 2021",
+    channel: {
+      cId: 1,
+      name: "Kurzgesagt - In a Nutshell",
+      logo: "https://yt3.ggpht.com/ytc/AKedOLQO2-DfuqwtPYTqBcZ8rEWt8g4p1sB_LyddKS5msQ=s88-c-k-c0x00ffffff-no-rj",
+    }
+  },
 
-    { id: 9, videoId: "BickMFHAZR0" },
-    { id: 10, videoId: "tH2tKigOPBU" },
-    { id: 11, videoId: "DTvS9lvRxZ8" },
-    { id: 12, videoId: "xaQJbozY_Is" },
-    { id: 13, videoId: "e-P5IFTqB98" },
-    { id: 14, videoId: "pP44EPBMb8A" },
-    
-    { id: 15, videoId: "NtQkz0aRDe8" }, 
-    { id: 16, videoId: "lARpY0nIQx0" }, 
-  ];
+  { id: 2,
+    videoId: "SUelbSa-OkA",
+    videoTitle: "LIFE BEYOND: Chapter 1. Alien life, deep time, and our place in cosmic history (4K)    ",
+    views: "10,867,712",
+    date: "Nov 20, 2019",
+    channel: {
+      cId: 2,
+      name: "melodysheep",
+      logo: "https://yt3.ggpht.com/ytc/AKedOLQX01z9397d0IgR7uW_Spo6odKET6mPUe0ChNb5cg=s88-c-k-c0x00ffffff-no-rj",
+    }
+  },
+
+  { id: 3,
+    videoId: "saWNMPL5ygk",
+    videoTitle: "LIFE BEYOND 3: In Search of Giants. The Hunt for Intelligent Alien Life (4K)    ",
+    views: "3,820,918",
+    date: "Dec 7, 2021",
+    channel: {
+      cId: 2,
+      name: "melodysheep",
+      logo: "https://yt3.ggpht.com/ytc/AKedOLQX01z9397d0IgR7uW_Spo6odKET6mPUe0ChNb5cg=s88-c-k-c0x00ffffff-no-rj",
+    }
+  },
+
+  { id: 4,
+    videoId: "ThDYazipjSI",
+    videoTitle: "LIFE BEYOND II: The Museum of Alien Life (4K)",
+    views: "15,555,869",
+    date: "Oct 7, 2020",
+    channel: {
+      cId: 2,
+      name: "melodysheep",
+      logo: "https://yt3.ggpht.com/ytc/AKedOLQX01z9397d0IgR7uW_Spo6odKET6mPUe0ChNb5cg=s88-c-k-c0x00ffffff-no-rj",
+    }
+  },
+
   
-  // https://www.youtube.com/watch?v=DTvS9lvRxZ8
-  // https://www.youtube.com/watch?v=wNDGgL73ihY
-  // https://www.youtube.com/watch?v=e-P5IFTqB98
-  // https://www.youtube.com/watch?v=pP44EPBMb8A
-  // https://www.youtube.com/watch?v=NtQkz0aRDe8
-  // https://www.youtube.com/watch?v=lARpY0nIQx0
-  // https://www.youtube.com/watch?v=_1R_pg5QasA
+  { id: 5,
+    videoId: "_1R_pg5QasA",
+    videoTitle: "When Will COVID End?",
+    views: "3,124,896",
+    date: "Jan 19, 2022",
+    channel: {
+      cId: 3,
+      name: "AsapSCIENCE",
+      logo: "https://yt3.ggpht.com/ytc/AKedOLSrnSwLN83-A6pQxTI-8kQkadSueJ0iMeltGiGnTA=s88-c-k-c0x00ffffff-no-rj",
+    }
+  },
+
+  { id: 6,
+    videoId: "6SrsZVdU740",
+    videoTitle: "The Secret History of the Moon - 4K",
+    views: "1,712,043",
+    date: "Apr 30, 2020",
+    channel: {
+      cId: 2,
+      name: "melodysheep",
+      logo: "https://yt3.ggpht.com/ytc/AKedOLQX01z9397d0IgR7uW_Spo6odKET6mPUe0ChNb5cg=s88-c-k-c0x00ffffff-no-rj",
+    }
+  },
+
+  { id: 7,
+    videoId: "uD4izuDMUQA",
+    videoTitle: "TIMELAPSE OF THE FUTURE: A Journey to the End of Time (4K)",
+    views: "78,439,473",
+    date: "Mar 20, 2019",
+    channel: {
+      cId: 2,
+      name: "melodysheep",
+      logo: "https://yt3.ggpht.com/ytc/AKedOLQX01z9397d0IgR7uW_Spo6odKET6mPUe0ChNb5cg=s88-c-k-c0x00ffffff-no-rj",
+    }
+  },
+
+  { id: 8,
+    videoId: "dFCbJmgeHmA",
+    videoTitle: "The Day the Dinosaurs Died – Minute by Minute    ",
+    views: "13,060,384",
+    date: "Jun 15, 2021",
+    channel: {
+      cId: 1,
+      name: "Kurzgesagt - In a Nutshell",
+      logo: "https://yt3.ggpht.com/ytc/AKedOLQX01z9397d0IgR7uW_Spo6odKET6mPUe0ChNb5cg=s88-c-k-c0x00ffffff-no-rj",
+    }
+  },
+
+  { id: 9,
+    videoId: "BickMFHAZR0",
+    videoTitle: "How Trees Bend the Laws of Physics    ",
+    views: "4,017,528",
+    date: "Oct 30, 2012",
+    channel: {
+      cId: 4,
+      name: "Veritasium",
+      logo: "https://yt3.ggpht.com/ytc/AKedOLQYcLqrGZC1RZL4e1AFz5-ImMQmmgObvzWBCL7-mA=s88-c-k-c0x00ffffff-no-rj",
+    }
+  },
+
+  { id: 10,
+    videoId: "tH2tKigOPBU",
+    videoTitle: "Perseverance Mars Rover Landing- Inside Story    ",
+    views: "13,658,884",
+    date: "Feb 15, 2021",
+    channel: {
+      cId: 5,
+      name: "Mark Rober",
+      logo: "https://yt3.ggpht.com/ytc/AKedOLROoLxOPO5oOgstWUmtYIEryw050SUJWHptwGWh=s88-c-k-c0x00ffffff-no-rj",
+    }
+  },
+
+  { id: 11,
+    videoId: "DTvS9lvRxZ8",
+    videoTitle: "Backyard Squirrel Maze 2.0- The Walnut Heist    ",
+    views: "53,384,203",
+    date: "May 19, 2021",
+    channel: {
+      cId: 5,
+      name: "Mark Rober",
+      logo: "https://yt3.ggpht.com/ytc/AKedOLROoLxOPO5oOgstWUmtYIEryw050SUJWHptwGWh=s88-c-k-c0x00ffffff-no-rj",
+    }
+  },
+
+  { id: 12,
+    videoId: "xaQJbozY_Is",
+    videoTitle: "What Dinosaurs ACTUALLY Looked Like?    ",
+    views: "8,706,402",
+    date: "Oct 12, 2021",
+    channel: {
+      cId: 1,
+      name: "Kurzgesagt - In a Nutshell",
+      logo: "https://yt3.ggpht.com/ytc/AKedOLQX01z9397d0IgR7uW_Spo6odKET6mPUe0ChNb5cg=s88-c-k-c0x00ffffff-no-rj",
+    }
+  },
+
+  { id: 13,
+    videoId: "e-P5IFTqB98",
+    videoTitle: "Black Holes Explained – From Birth to Death    ",
+    views: "19,347,752",
+    date: "Dec 15, 2015",
+    channel: {
+      cId: 1,
+      name: "Kurzgesagt - In a Nutshell",
+      logo: "https://yt3.ggpht.com/ytc/AKedOLQX01z9397d0IgR7uW_Spo6odKET6mPUe0ChNb5cg=s88-c-k-c0x00ffffff-no-rj",
+    }
+  },
+
+  { id: 14,
+    videoId: "pP44EPBMb8A",
+    videoTitle: "How to Build a Dyson Sphere - The Ultimate Megastructure    ",
+    views: "17,889,672",
+    date: "Dec 20, 2018",
+    channel: {
+      cId: 1,
+      name: "Kurzgesagt - In a Nutshell",
+      logo: "https://yt3.ggpht.com/ytc/AKedOLQX01z9397d0IgR7uW_Spo6odKET6mPUe0ChNb5cg=s88-c-k-c0x00ffffff-no-rj",
+    }
+  },
+
+  { id: 15,
+    videoId: "NtQkz0aRDe8",
+    videoTitle: "How We Could Build a Moon Base TODAY – Space Colonization 1    ",
+    views: "13,286,914",
+    date: "Sep 16, 2018",
+    channel: {
+      cId: 1,
+      name: "Kurzgesagt - In a Nutshell",
+      logo: "https://yt3.ggpht.com/ytc/AKedOLQX01z9397d0IgR7uW_Spo6odKET6mPUe0ChNb5cg=s88-c-k-c0x00ffffff-no-rj",
+    }
+  },
+
+  { id: 16,
+    videoId: "lARpY0nIQx0",
+    videoTitle: "Is NASA a waste of money?    ",
+    views: "13,588,839",
+    date: "Feb 15, 2018",
+    channel: {
+      cId: 5,
+      name: "Mark Rober",
+      logo: "https://yt3.ggpht.com/ytc/AKedOLROoLxOPO5oOgstWUmtYIEryw050SUJWHptwGWh=s88-c-k-c0x00ffffff-no-rj",
+    }
+  },
+
+ ];
+
+// https://www.youtube.com/watch?v=DTvS9lvRxZ8
+// https://www.youtube.com/watch?v=wNDGgL73ihY
+// https://www.youtube.com/watch?v=e-P5IFTqB98
+// https://www.youtube.com/watch?v=pP44EPBMb8A
+// https://www.youtube.com/watch?v=NtQkz0aRDe8
+// https://www.youtube.com/watch?v=lARpY0nIQx0
+// https://www.youtube.com/watch?v=_1R_pg5QasA

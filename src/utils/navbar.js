8fbecdbf8ad0bb2c0102f28405001e88021f21c3
MadhushreeKunder <madhushreekunder@gmail.com>
2022-01-28 20:28:26 +0530

    darken textcolor

--- src/utils/navbar.js
+++ src/utils/navbar.js
@@ -10,7 +10,7 @@ import {
 export const NavBar = () => {
   return (
     <aside className="fixed left-0 h-full z-1">
-      <nav className=" flex flex-col items-start pl-3 pr-3 mr-20 h-full bg-gray-800 text-gray-300 ">
+      <nav className=" flex flex-col items-start pl-3 pr-3 mr-20 h-full bg-gray-800 text-gray-400 ">
         <ul>
           <li className="px-3 py-2 my-2 text-lg block hover:bg-gray-700 active:bg-gray-700  focus:bg-gray-700 active:text-white focus:text-white rounded">
             <Link to="/">

2b3d9f69884d63866bfe3b5048a751239d21a8ac
MadhushreeKunder <madhushreekunder@gmail.com>
2022-01-28 16:18:55 +0530

    styling done

--- src/Pages/videoPlayerPage.js
+++ src/Pages/videoPlayerPage.js
@@ -3,7 +3,8 @@ import React from "react";
 import { videosDb } from "../database";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
-import { FaDotCircle } from "react-icons/fa";
+import { FaDotCircle, FaClock, FaThumbsUp, FaFolderPlus, FaShareAlt
+} from "react-icons/fa";
 
 export function VideoPlayerPage() {
   const { videoID } = useParams();
@@ -21,14 +22,34 @@ export function VideoPlayerPage() {
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowfullscreen
         ></iframe>
-        <p className="text-white">Details</p>
+
+        <div className="flex flex-row mt-4 justify-between items-center">
+          <div className="flex flex-row gap-4">
+            <FaDotCircle className="text-white text-3xl" />
+
+            <div className="flex flex-col">
+              <p className="text-2xl font-medium text-white">Video Title</p>
+              <div className="flex flex-row gap-3 text-gray-400">
+                <p>Video channnel</p>
+                <p>1.5k views</p>
+                <p>Date</p>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-row gap-6 text-2xl text-gray-300 mr-4">
+            <FaThumbsUp className="active:text-white hover:text-white focus:text-rose-500"/>
+            <FaClock className="active:text-white hover:text-white focus:text-rose-500"/>
+            <FaFolderPlus className="active:text-white hover:text-white focus:text-rose-500"/>
+            <FaShareAlt className="active:text-white hover:text-white focus:text-rose-500"/>
+          </div>
+        </div>
       </div>
 
       <div>
         <ul className="flex flex-col flex-wrap justify-evenly flex:none">
           {videosDb.map((item) => (
             <li className="w-64 mx-2 mb-4">
-              <Link to={`/video/${item.videoId}`} >
+              <Link to={`/video/${item.videoId}`}>
                 <img
                   className="video-thumbnail"
                   src={`https://img.youtube.com/vi/${item.videoId}/maxresdefault.jpg`}

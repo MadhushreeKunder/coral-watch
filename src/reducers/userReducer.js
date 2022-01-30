d158c6bf7f0a63f830c2db88e4d27f3adb85a7b4
MadhushreeKunder <madhushreekunder@gmail.com>
2022-01-30 18:53:57 +0530

    add liked

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,33 +1,32 @@
-export const userReducer = (userState, action) => {
+export const userReducer = (state, action) => {
     switch (action.type) {
-      case "ADD_USER":
-        return {
-          _id: action.payload,
-          liked: [],
-          history: [],
-          watchLater: [],
-          playlists: [],
-          loading: "",
-        };
-  
+      
       case "LOAD_USER_ACTIVITY":
         return {
           ...action.payload,
-          loading: "",
+          // loading: "",
           liked: action.payload.liked.map((item) => {return item.videoId}),
           history: action.payload.history.map((item) => {return item.videoId}),
           watchLater: action.payload.watchLater.map((item) => {return item.videoId}),
           playlists: action.payload.playlists.map((item) => {return item.videoId}),
         };
   
-      case "STATUS":
+      // case "STATUS":
+      //   return {
+      //     ...userState,
+      //     loading: action.payload,
+      //   };
+
+      case "LIKED":
         return {
-          ...userState,
-          loading: action.payload,
-        };
+          ...state, liked: state.liked.concat(action.payload)
+        }
+
+
+
   
       default:
-        return userState;
+        return state;
     }
   };
   

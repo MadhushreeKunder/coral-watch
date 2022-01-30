7cb2455de38dd9f79b7499e6f6711339dd21e266
MadhushreeKunder <madhushreekunder@gmail.com>
2022-01-30 18:48:29 +0530

    add loading, add functionality

--- src/Pages/signUp.js
+++ src/Pages/signUp.js
@@ -1,8 +1,8 @@
 import { useLocation, useNavigate } from "react-router";
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import { useUser } from "../contexts/userContext";
-import { useAuth } from "../contexts/authContext";
+import { useAuth, useUser } from "../contexts";
+import { FaUser, FaLock, FaEye, FaEyeSlash, FaEnvelope } from "react-icons/fa";
 
 export const SignUp = () => {
   const { status, signUpUserWithCreds } = useAuth();
@@ -62,142 +62,144 @@ export const SignUp = () => {
   };
 
   return (
-    <div className="login-form">
-      <h2>Sign Up</h2>
-      <form onSubmit={(e) => e.preventDefault()} className="signup-form">
-        <div className="login-input">
-          <label>
-            <i className="fas fa-envelope"></i> Email
-          </label>
-          <input
-            type="email"
-            placeholder="Enter email"
-            className="form-input"
-            required
-            value={signUpCredentials.email}
-            onChange={(e) =>
-              setSignUpCredentials(() => ({
-                ...signUpCredentials,
-                message: "",
-                email: e.target.value,
-              }))
-            }
-          />
-        </div>
-
-        <div className="login-input">
-          <label>
-            <i className="fas fa-user"></i> Username
-          </label>
-          <input
-            type="text"
-            placeholder="Enter username"
-            className="form-input"
-            required
-            value={signUpCredentials.username}
-            onChange={(e) =>
-              setSignUpCredentials(() => ({
-                ...signUpCredentials,
-                message: "",
-                username: e.target.value,
-              }))
-            }
-          />
-        </div>
-
-        <div className="login-input">
-          <label>
-            <i className="fas fa-lock"></i> Password
-          </label>
-          <input
-            className="form-input"
-            placeholder="Enter password"
-            required
-            type={signUpCredentials.showPassword ? "text" : "password"}
-            value={signUpCredentials.password}
-            onChange={(e) =>
-              setSignUpCredentials(() => ({
-                ...signUpCredentials,
-                message: "",
-                password: e.target.value,
-              }))
-            }
-          />
-
-          <button
-            className="login-show-password"
-            onClick={() =>
-              setSignUpCredentials(() => ({
-                ...signUpCredentials,
-                showPassword: !signUpCredentials.showPassword,
-              }))
-            }
-          >
-            {signUpCredentials.showPassword ? (
-              <i className="far fa-lg fa-eye"></i>
-            ) : (
-              <i className="far fa-lg fa-eye-slash"></i>
-            )}
-          </button>
-        </div>
-
-        <div className="login-input">
-          <label>
-            <i className="fas fa-lock"></i> Confirm Password
-          </label>
-          <input
-            className="form-input"
-            placeholder="Confirm Password"
-            required
-            type={signUpCredentials.showConfirmPassword ? "text" : "password"}
-            value={signUpCredentials.confirmPassword}
-            onChange={(e) =>
-              setSignUpCredentials(() => ({
-                ...signUpCredentials,
-                message: "",
-                confirmPassword: e.target.value,
-              }))
-            }
-          />
-
-          <button
-            className="login-show-password"
-            onClick={() =>
-              setSignUpCredentials(() => ({
-                ...signUpCredentials,
-                showConfirmPassword: !signUpCredentials.showConfirmPassword,
-              }))
-            }
-          >
-            {signUpCredentials.showConfirmPassword ? (
-              <i className="far fa-lg fa-eye"></i>
-            ) : (
-              <i className="far fa-lg fa-eye-slash"></i>
-            )}
-          </button>
-        </div>
-
-        <p>{signUpCredentials.message}</p>
-
-        <button
-          className="button button-primary login-button"
-          onClick={signUpUser}
+    <div className="h-screen mt-4 pt-7">
+      <div className="p-8 w-max flex flex-col items-center m-auto border-2 border-solid border-slate-800 rounded-lg text-white">
+        <h1 className="text-white text-2xl font-semibold">SignUp</h1>{" "}
+        <h3 className="fixed z-10 pt-40  top-0  h-full overflow-auto bg-opacity-10">
+          {status?.loading && (
+            <img src="/Images/Loading-blue.svg" alt="loading" />
+          )}
+        </h3>
+        <form
+          onSubmit={(e) => e.preventDefault()}
+          className="flex flex-col items-center justify-center align-middle my-4 mx-auto"
         >
-          Sign Up
-        </button>
+          <div className="flex flex-col w-60 m-4">
+            <label className="mb-2 text-gray-300">
+              <FaEnvelope className="inline" /> Email
+            </label>
+            <input
+              type="email"
+              placeholder="Enter email"
+              className="text-slate-900 font-medium p-2"
+              required
+              value={signUpCredentials.email}
+              onChange={(e) =>
+                setSignUpCredentials(() => ({
+                  ...signUpCredentials,
+                  email: e.target.value,
+                }))
+              }
+            />
+          </div>
 
-        <small className="mg">
-          Already have an account?{" "}
-          <Link to="/login">
-            <span className="login-signup">Login!</span>
-          </Link>
-        </small>
-      </form>
-      <h3>
-        {status?.loading && (
-          <img src="/images/loading.svg" alt="loading" className="loading" />
-        )}
-      </h3>
+          <div className="flex flex-col w-60 m-4">
+            <label className="mb-2 text-gray-300">
+              <FaUser className="inline" /> Username
+            </label>
+            <input
+              type="text"
+              placeholder="Enter username"
+              className="text-slate-900 font-medium  p-2"
+              required
+              value={signUpCredentials.username}
+              onChange={(e) =>
+                setSignUpCredentials(() => ({
+                  ...signUpCredentials,
+                  username: e.target.value,
+                }))
+              }
+            />
+          </div>
+
+          <div className="flex flex-col w-60 m-4 relative">
+            <label className="mb-2 text-gray-300">
+              <FaLock className="inline" /> Password
+            </label>
+            <input
+              className="text-slate-900 font-medium  p-2 "
+              placeholder="Enter password"
+              required
+              type={signUpCredentials.showPassword ? "text" : "password"}
+              value={signUpCredentials.password}
+              onChange={(e) =>
+                setSignUpCredentials(() => ({
+                  ...signUpCredentials,
+                  password: e.target.value,
+                }))
+              }
+            />
+
+            <button
+              className="text-slate-900 absolute right-3 top-11"
+              onClick={() =>
+                setSignUpCredentials(() => ({
+                  ...signUpCredentials,
+                  showPassword: !signUpCredentials.showPassword,
+                }))
+              }
+            >
+              {signUpCredentials.showPassword ? <FaEye /> : <FaEyeSlash />}{" "}
+            </button>
+          </div>
+
+          <div className="flex flex-col w-60 m-4 relative">
+            <label className="mb-2 text-gray-300">
+              <FaLock className="inline" /> Confirm Password
+            </label>
+            <input
+              className="text-slate-900 font-medium  p-2"
+              placeholder="Confirm Password"
+              required
+              type={signUpCredentials.showConfirmPassword ? "text" : "password"}
+              value={signUpCredentials.confirmPassword}
+              onChange={(e) =>
+                setSignUpCredentials(() => ({
+                  ...signUpCredentials,
+                  confirmPassword: e.target.value,
+                }))
+              }
+            />
+
+            <button
+              className="text-slate-900 absolute right-3 top-11"
+              onClick={() =>
+                setSignUpCredentials(() => ({
+                  ...signUpCredentials,
+                  showConfirmPassword: !signUpCredentials.showConfirmPassword,
+                }))
+              }
+            >
+              {signUpCredentials.showConfirmPassword ? (
+                <FaEye />
+              ) : (
+                <FaEyeSlash />
+              )}
+            </button>
+          </div>
+
+          <p>{signUpCredentials.message}</p>
+
+          <button
+            className="py-2 px-4 m-4 block w-fit rounded-lg bg-cyan-500 shadow-lg shadow-cyan-500/50 active:shadow-gray-900 text-slate-900 font-bold"
+            onClick={signUpUser}
+          >
+            Sign Up
+          </button>
+
+          <small className="text-base">
+            Already have an account?{" "}
+            <Link to="/login">
+              <span className="text-sky-400 hover:underline">Login!</span>
+            </Link>
+          </small>
+        </form>
+        {/* <h3>
+          {status?.loading && (
+            <img src="/Images/Loading-blue.svg" alt="loading" />
+          )}
+        </h3> */}
+      </div>
     </div>
   );
 };

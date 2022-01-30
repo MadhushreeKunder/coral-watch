44d2c1de55c498391a46bdb7afae0f71181d090e
MadhushreeKunder <madhushreekunder@gmail.com>
2022-01-30 18:53:38 +0530

    add add_data

---
+++ src/reducers/videoReducer.js
@@ -0,0 +1,9 @@
+export const videoReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_DATA":
+      return { ...state, data: action.payload };
+
+    default:
+      return state;
+  }
+};

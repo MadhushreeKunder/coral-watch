456554cccea6197f8935f2f1eba02a7361cfdee4
MadhushreeKunder <madhushreekunder@gmail.com>
2022-01-31 20:01:20 +0530

    item --> video

--- src/Pages/videoListing.js
+++ src/Pages/videoListing.js
@@ -1,44 +1,42 @@
-// import "../styles.css";
-
-import { videosDb } from "../database";
 import { Link, useNavigate } from "react-router-dom";
 import { viewsFormatter } from "../utils/viewsFormatter";
 import { useUser, useVideo, useAuth} from "../contexts";
+import { addToHistory } from "../utils/apiSync";
 
 
 export function VideoListing() {
   const {userState, userDispatch} = useUser();
   const { data, status } = useVideo();
-  const {token} = useAuth();
-  const navigate = useNavigate();
-
+  const {token, user} = useAuth();
 
   return (
     <div className="h-full ml-44 mt-4 py-8 bg-gray-900">
       <ul className="flex flex-row flex-wrap justify-evenly">
-        {data.map((item) => (
+        {data.map((video) => (
           <li className="w-64 mx-2 mb-4" >
-            <Link to={`/video/${item._id}`} >
+            <Link to={`/video/${video._id}`} >
+              <div onClick={() => addToHistory(user, video, userDispatch) } key={video._id}>
               <img
                 className="video-thumbnail"
-                src={`https://img.youtube.com/vi/${item._id}/maxresdefault.jpg`}
+                src={`https://img.youtube.com/vi/${video._id}/maxresdefault.jpg`}
                 alt="video-name"
               />
 
               <div className="flex flex-row mt-2 gap-2">
                 {/* <FaDotCircle className="text-white text-3xl" /> */}
                 <img
-                  src={item.channel.logo}
-                  alt={item.videoTitle}
+                  src={video.channel.logo}
+                  alt={video.videoTitle}
                   className="rounded-full h-8"
                 ></img>
                 <div className="flex flex-col">
-                  <p className="text-gray-100 font-medium">{item.videoTitle}</p>
-                  <small className="text-gray-400">{item.channel.name}</small>
+                  <p className="text-gray-100 font-medium">{video.videoTitle}</p>
+                  <small className="text-gray-400">{video.channel.name}</small>
                   <small className="text-gray-400">
-                    {viewsFormatter(item.views)} views • {item.date}
+                    {viewsFormatter(video.views)} views • {video.date}
                   </small>
                 </div>
+              </div>
               </div>
             </Link>
           </li>

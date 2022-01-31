dd823e74c49d1f2ef1b09a64103a1cf0a4a2dbdf
MadhushreeKunder <madhushreekunder@gmail.com>
2022-01-31 17:54:27 +0530

    add remove liked

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -3,24 +3,34 @@ export const userReducer = (state, action) => {
       
       case "LOAD_USER_ACTIVITY":
         return {
-          ...action.payload,
+          // ...action.payload,
           // loading: "",
-          liked: action.payload.liked.map((item) => {return item.videoId}),
-          history: action.payload.history.map((item) => {return item.videoId}),
-          watchLater: action.payload.watchLater.map((item) => {return item.videoId}),
-          playlists: action.payload.playlists.map((item) => {return item.videoId}),
+          // liked: action.payload.liked.map((item) => {return item.videoId}),
+          // history: action.payload.history.map((item) => {return item.videoId}),
+          // watchLater: action.payload.watchLater.map((item) => {return item.videoId}),
+          // playlists: action.payload.playlists.map((item) => {return item.videoId}),
+          liked: action.payload.liked,
+          history: action.payload.history,
+          watchLater: action.payload.watchLater,
+          playlists: action.payload.playlists,
         };
   
-      // case "STATUS":
-      //   return {
-      //     ...userState,
-      //     loading: action.payload,
-      //   };
+      case "STATUS":
+        return {
+          ...state,
+          loading: action.payload,
+        };
 
       case "LIKED":
         return {
           ...state, liked: state.liked.concat(action.payload)
         }
+
+      case "LIKED_REMOVE":
+        return {...state, liked: state.liked.filter((item) => {
+          return item.videoId._id !== action.payload._id;
+        })}
+      
 
 
 

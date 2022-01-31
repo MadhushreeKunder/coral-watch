c27b5ca40b941fdb2330723962e62af84d3953d6
MadhushreeKunder <madhushreekunder@gmail.com>
2022-01-31 22:25:27 +0530

    change color for watch later

--- src/utils/toggleColor.js
+++ src/utils/toggleColor.js
@@ -2,8 +2,15 @@ export const likeToggle = (item, userState, token) => {
     return token
       ? userState.liked.reduce((acc, value) => {
           return value.videoId._id === item._id ? "text-sky-400" : acc;
-        }, "hover:text-white text-gray-300")
-      : "hover:text-white text-gray-300";
+        }, " text-gray-400 hover:text-white")
+      : " text-gray-400 hover:text-white";
   };
   
+export const watchlaterToggle = (item, userState, token) => {
+  return token
+    ? userState.watchlater.reduce((acc, value) => {
+        return value.videoId._id === item._id ? "text-sky-400" : acc;
+      }, " text-gray-400 hover:text-white")
+    : " text-gray-400 hover:text-white";
+};
 

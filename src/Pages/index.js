1c2922d12f19a1f7e6b6ac4d0d1410d8264e2670
MadhushreeKunder <madhushreekunder@gmail.com>
2022-01-31 20:34:14 +0530

    spacing

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,9 +1,9 @@
 export { VideoListing } from "./videoListing";
 export { Playlists } from "./playlists";
 export { LikedVideos } from "./likedVideos";
-export { WatchLater } from "./watchLater";
+export { WatchLater } from "./watchlater";
 export { History } from "./history";
 export { VideoPlayerPage } from "./videoPlayerPage";
 export { Login } from "./login";
 export { SignUp } from "./signUp";
-export {LogoutUser} from "./logoutUser";
+export { LogoutUser } from "./logoutUser";

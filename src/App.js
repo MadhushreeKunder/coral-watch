6e42c41e423defe291748f0ab571020f00d8bd95
MadhushreeKunder <madhushreekunder@gmail.com>
2022-01-31 17:56:48 +0530

    add logout user

--- src/App.js
+++ src/App.js
@@ -8,24 +8,26 @@ import {
   VideoPlayerPage,
   SignUp,
   Login,
+  LogoutUser
 } from "./Pages";
 import { Header, NavBar } from "./utils";
 import { Link, Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
-    <div className="App bg-gray-900">
+    <div className="App bg-gray-900 h-full">
       <Header />
       <NavBar />
       <Routes className="routes">
         <Route path="/" element={<VideoListing />} />
         <Route path="/playlists" element={<Playlists />} />
-        <Route path="/video/:videoID" element={<VideoPlayerPage />} />
+        <Route path="/video/:videoId" element={<VideoPlayerPage />} />
         <Route path="/likedvideos" element={<LikedVideos />} />
         <Route path="/watchlater" element={<WatchLater />} />
         <Route path="/history" element={<History />} />
         <Route path="/signup" element={<SignUp/>} />
         <Route path="/login" element={<Login/>} />
+        <Route path="/logout" element={<LogoutUser/>}></Route>
       </Routes>
     </div>
   );

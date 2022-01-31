1ac3a347d64a087abbb8fc45fff47bb64561184b
MadhushreeKunder <madhushreekunder@gmail.com>
2022-01-31 14:33:53 +0530

    style login

--- src/Pages/login.js
+++ src/Pages/login.js
@@ -3,6 +3,8 @@ import { useAuth } from "../contexts/authContext";
 import { useLocation, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { FaUser, FaLock, FaEye, FaEyeSlash, FaEnvelope } from "react-icons/fa";
+
 
 export function Login() {
   const { status, loginUserWithCreds } = useAuth();
@@ -16,7 +18,6 @@ export function Login() {
   });
 
   const loginUser = async () => {
-
     if (loginCredentials.username && loginCredentials.password) {
       const result = await loginUserWithCreds(
         loginCredentials.username,
@@ -35,78 +36,93 @@ export function Login() {
   };
 
   return (
-    <div className="login-form">
-      <h2>Login</h2>
-      <div className="login-input">
-        <label>
-          <i className="fas fa-user"></i> Username
-        </label>
-        <input
-          type="text"
-          placeholder="Enter username"
-          className="form-input"
-          required
-          value={loginCredentials.username}
-          onChange={(e) =>
-            setLoginCredentials(() => ({
-              ...loginCredentials,
-              msg: "",
-              username: e.target.value,
-            }))
-          }
-        />
-      </div>
-
-      <div className="login-input ">
-        <label>
-          <i className="fas fa-lock"></i> Password
-        </label>
-        <input
-          type={loginCredentials.showPassword ? "text" : "password"}
-          placeholder="Enter password"
-          className="form-input"
-          required
-          value={loginCredentials.password}
-          onChange={(e) =>
-            setLoginCredentials(() => ({
-              ...loginCredentials,
-              msg: "",
-              password: e.target.value,
-            }))
-          }
-        />
-
-        <button
-          className="login-show-password"
-          onClick={() =>
-            setLoginCredentials(() => ({
-              ...loginCredentials,
-              showPassword: !loginCredentials.showPassword,
-            }))
-          }
-        >
-          {loginCredentials.showPassword ? (
-            <i className="far fa-lg fa-eye"></i>
-          ) : (
-            <i className="far fa-lg  fa-eye-slash"></i>
+    <div className="h-screen mt-4 pt-7">
+      <div className="p-8 w-max flex flex-col items-center m-auto border-2 border-solid border-slate-800 rounded-lg text-white">
+        <h2 className="text-white text-2xl font-semibold">Login</h2>
+        <h3 className="fixed z-10 pt-40  top-0  h-full overflow-auto bg-opacity-10">
+          {status?.loading && (
+            <img src="/Images/Loading-blue.svg" alt="loading" />
           )}
+        </h3>
+        <form
+          onSubmit={(e) => e.preventDefault()}
+          className="flex flex-col items-center justify-center align-middle my-4 mx-auto"
+        >
+        <div className="flex flex-col w-60 m-4">
+          <label className="mb-2 text-gray-300">
+          <FaUser className="inline" /> Username
+          </label>
+          <input
+            type="text"
+            placeholder="Enter username"
+            className="text-slate-900 font-medium  p-2 "
+            required
+            value={loginCredentials.username}
+            onChange={(e) =>
+              setLoginCredentials(() => ({
+                ...loginCredentials,
+                msg: "",
+                username: e.target.value,
+              }))
+            }
+          />
+        </div>
+
+        <div className="flex flex-col w-60 m-4 relative">
+          <label  className="mb-2 text-gray-300">
+          <FaLock className="inline" />  Password
+          </label>
+          <input
+            type={loginCredentials.showPassword ? "text" : "password"}
+            placeholder="Enter password"
+            className="text-slate-900 font-medium  p-2 "
+            required
+            value={loginCredentials.password}
+            onChange={(e) =>
+              setLoginCredentials(() => ({
+                ...loginCredentials,
+                msg: "",
+                password: e.target.value,
+              }))
+            }
+          />
+
+          <button
+              className="text-slate-900 absolute right-3 top-11"
+              onClick={() =>
+              setLoginCredentials(() => ({
+                ...loginCredentials,
+                showPassword: !loginCredentials.showPassword,
+              }))
+            }
+          >
+            {loginCredentials.showPassword ? <FaEye /> : <FaEyeSlash />}
+          </button>
+        </div>
+        {/* <h3>
+          {status.loading && (
+            <img
+              src="../images/loading.svg"
+              alt="loading"
+              className="loading"
+            />
+          )}
+        </h3> */}
+        <p>{loginCredentials.msg}</p>
+        <button
+            className="py-2 px-4 m-4 block w-fit rounded-lg bg-cyan-500 shadow-lg shadow-cyan-500/50 active:shadow-gray-900 text-slate-900 font-bold"
+            onClick={loginUser}
+        >
+          Login
         </button>
+        <small className="text-base">
+          Don't have an account?{" "}
+          <Link to="/signup">
+            <span className="text-sky-400 hover:underline"> Sign up!</span>
+          </Link>
+        </small>
+        </form>
       </div>
-      <h3>
-        {status.loading && (
-          <img src="../images/loading.svg" alt="loading" className="loading" />
-        )}
-      </h3>
-      <p>{loginCredentials.msg}</p>
-      <button className="button button-primary login-button" onClick={loginUser}>
-        Login
-      </button>
-      <small>
-        Don't have an account?{" "}
-        <Link to="/signup">
-          <span className="login-signup"> Sign up!</span>
-        </Link>
-      </small>
     </div>
   );
 }

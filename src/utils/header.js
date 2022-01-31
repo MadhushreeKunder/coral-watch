639d9ed1c64eee6364e9348deaf9e17eb3d4aa28
MadhushreeKunder <madhushreekunder@gmail.com>
2022-01-31 17:52:46 +0530

    done

--- src/utils/header.js
+++ src/utils/header.js
@@ -1,8 +1,12 @@
 import { NavLink, Link } from "react-router-dom";
+import { useAuth, useUser } from "../contexts";
 
 export const Header = () => {
+  const { user, token, logout } = useAuth();
+  const { userState } = useUser();
+
   return (
-    <div className="relative mb-16">
+    <div className="relative mb-16 z-50">
       <header className="fixed top-0 left-0 right-0 bg-gray-800 p-4 flex flex-row justify-between items-center">
         <Link to="/">
           <div className="flex flex-row items-center gap-2">
@@ -12,12 +16,13 @@ export const Header = () => {
         </Link>
 
         <div className="flex flex-row gap-4">
-        <NavLink to="/login" className="font-bold text-white">Login</NavLink>
-        <NavLink to="/signup" className="font-bold text-white">Signup</NavLink>
-        <div className="font-bold text-white">Logout</div>
-        </div>
-
-    
+          <NavLink
+            className="font-bold text-white"
+            to= {token ? "/logout" : "/login"}
+            >
+            {token ? `Hello, ${user.username}` : "Login"}
+          </NavLink>
+        </div>  
       </header>
     </div>
   );

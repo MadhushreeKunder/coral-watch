a62b1986b05224ad14667be2713d25428021cb0f
MadhushreeKunder <madhushreekunder@gmail.com>
2022-01-31 20:00:06 +0530

    add watchlater

--- src/Pages/watchLater.js
+++ src/Pages/watchLater.js
@@ -3,6 +3,9 @@ import { Link } from "react-router-dom";
 import { FaDotCircle } from "react-icons/fa";
 import { viewsFormatter } from "../utils/viewsFormatter";
 import { useAuth, useUser } from "../contexts";
+import { removeFromWatchLater } from "../utils/apiSync";
+import { FaTimesCircle } from "react-icons/fa";
+
 
 // Add remove from watch later
 
@@ -11,28 +14,36 @@ export function WatchLater() {
   const { userState, userDispatch } = useUser();
 
   return (
-    <div className="h-full ml-44 mt-4 py-8">
+    <div className="h-screen ml-44 mt-4 py-8">
       <h1 className="text-white text-2xl font-semibold ml-5 mb-5">
         Watch Later
       </h1>
       <ul className="flex flex-row flex-wrap justify-evenly">
-        {videosDb.map((item) => (
-          <li className="w-64 mx-2 mb-4">
-            <Link to={`/video/${item.videoLinkId}`}>
+        {userState.watchLater.map(({videoId: video}) => (
+          <li className="w-64 mx-2 mb-4 relative">
+             <button
+              className="text-2xl text-white absolute -top-4 -right-4 z-2"
+              onClick={() => {
+                removeFromWatchLater(user, video, userDispatch);
+              }}
+            >
+              <FaTimesCircle />
+            </button>
+            <Link to={`/video/${video._id}`}>
               <img
                 className="video-thumbnail"
-                src={`https://img.youtube.com/vi/${item.videoLinkId}/maxresdefault.jpg`}
+                src={`https://img.youtube.com/vi/${video._id}/maxresdefault.jpg`}
                 alt="video-name"
               />
 
               <div className="flex flex-row mt-2 gap-2">
                 {/* <FaDotCircle className="text-white text-3xl" /> */}
-                <img src={item.channel.logo} className="rounded-md h-8"></img>
+                <img src={video.channel.logo} alt={video.channel.name} className="rounded-md h-8"></img>
                 <div className="flex flex-col">
-                  <p className="text-gray-100 font-medium">{item.videoTitle}</p>
-                  <small className="text-gray-400">{item.channel.name}</small>
+                  <p className="text-gray-100 font-medium">{video.videoTitle}</p>
+                  <small className="text-gray-400">{video.channel.name}</small>
                   <small className="text-gray-400">
-                    {viewsFormatter(item.views)} views . {item.date}
+                    {viewsFormatter(video.views)} views . {video.date}
                   </small>
                 </div>
               </div>
